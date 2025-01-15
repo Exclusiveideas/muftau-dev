624d94aa8b29d5ca07adf88abde847e659835612
Exclusiveideas <muftaudeenoluwatosin@gmail.com>
2025-01-15 18:51:25 +0100

    parse error deployment fix

--- src/projectPageComponents/titleSection/index.jsx
+++ src/projectPageComponents/titleSection/index.jsx
@@ -1,11 +1,11 @@
 import './titleSection.css';
 
 const TitleSection = ({ title, red}) => {
-  const splittedTitle = title?.split(red)
+  const splittedTitle = title?.split(red) ?? ['', '']
   return (
     <div className='titleSection-wrapper'>
         <p className="title-section-label">PROJECT TITLE</p>
-        <p className="projectTitle">{splittedTitle[0]} <span className='red'>{red}</span> {splittedTitle[1]}</p>
+        <p className="projectTitle">{splittedTitle[0] && splittedTitle[0]} <span className='red'>{red}</span> {splittedTitle[1]}</p>
     </div>
   )
 }
